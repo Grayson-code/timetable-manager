65664468e4382ba89d093f1fd1ac63ba9c6856fd
Cinnamon-8 <121104003+Cinnamon-8@users.noreply.github.com>
2023-05-27 05:11:54 +0000

    Added ui and fixed some errors

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,12 +1,94 @@
-// firebase.ts
-
+import { env } from '$env/dynamic/public';
 import { initializeApp } from 'firebase/app';
+import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import {
+	addDoc,
+	collection,
+	serverTimestamp,
+	connectFirestoreEmulator,
+	initializeFirestore,
+	CACHE_SIZE_UNLIMITED
+} from 'firebase/firestore';
 
-const firebaseConfig = {
-  projectId: 'YOUR_PROJECT_ID',
-  appId: 'YOUR_WEB_APP_ID'
+// import type { DocumentReference, DocumentData } from 'firebase/firestore';
+import type { FirebaseApp } from 'firebase/app';
+import { enableMultiTabIndexedDbPersistence } from 'firebase/firestore';
+
+const config = {
+	useEmulators: env?.PUBLIC_USE_EMULATORS || false,
+	region: env?.PUBLIC_REGION || '',
+	firebase: {
+		apiKey: env?.PUBLIC_FIREBASE_FIREBASE_APIKEY || 'AIzaSyAr6APB8WMEFw3I2dhVPOZswreZJnxJasI',
+		authDomain: env?.PUBLIC_FIREBASE_AUTH_DOMAIN || 'timetable-m.firebaseapp.com',
+		projectId: env?.PUBLIC_FIREBASE_PROJECT_ID || 'timetable-m',
+		storageBucket: env?.PUBLIC_FIREBASE_STORAGE_BUCKET || 'timetable-m.appspot.com',
+		messagingSenderId: env?.PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '644634162185',
+		appId: env?.PUBLIC_FIREBASE_APP_ID || '1:644634162185:web:3e2ac4c6d3e792167434d8'
+	}
 };
 
-const app = initializeApp(firebaseConfig);
+let app: FirebaseApp;
 
-export default app;
+let initializedFirestore: any = null;
+
+let connectedFirestoreEmulator = false;
+let connectedAuthEmulator = false;
+
+// These functions should be called in onMount when used in components
+export const auth = () => {
+	const auth = getAuth(app);
+
+	if (config.useEmulators && !connectedAuthEmulator) {
+		console.log('👓 Connecting Firebase Auth Emulator');
+		connectedAuthEmulator = true;
+		connectAuthEmulator(auth, 'http://localhost:9099');
+	}
+
+	return auth;
+};
+export const db = () => {
+	if (!initializedFirestore) {
+		console.log('🔥 Initializing Firestore');
+
+		initializedFirestore = initializeFirestore(app, {
+			cacheSizeBytes: CACHE_SIZE_UNLIMITED
+		});
+	}
+
+	if (config.useEmulators && !connectedFirestoreEmulator) {
+		console.log('👓 Connecting Firestore Emulator');
+		connectedFirestoreEmulator = true;
+		connectFirestoreEmulator(initializedFirestore, 'localhost', 8090);
+	}
+
+	return initializedFirestore;
+};
+
+export const initialize = async () => {
+	console.log('🔥 Initializing Firebase SDK');
+	console.log(config.firebase);
+	app = initializeApp(config.firebase);
+
+	try {
+		
+	} catch (err: any) {
+		
+		if (err?.code == 'failed-precondition') {
+			console.warn("Couldn't enable persistence", err);
+		} else if (err?.code == 'unimplemented') {
+			console.warn('Persistence mode not implemented', err);
+		} else {
+			console.warn('Persistence mode error: ', err);
+		}
+	}
+};
+
+export const addMessage = async (name: string, message: string) => {
+	const docRef = await addDoc(collection(db(), 'messages'), {
+		name,
+		message,
+		time: serverTimestamp() 
+	});
+	console.log('Chat message written with ID: ', docRef.id);
+	return docRef;
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,105 @@
-<
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { addMessage, initialize } from '$lib/firebase';
+	import { initUserStore, userStore, userProfileStore, loginUser } from '$lib/stores/user';
+	import { chatMessagesStore } from '$lib/stores/messages';
+	import Message from '$lib/components/Message.svelte';
+	import GithubBanner from '$lib/components/GithubBanner.svelte';
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<a href="/login">Go to Login Page</a>
-<a href="/signup">Go to Signup Page</a>
+	let message = '';
+	let chatInput: undefined | HTMLInputElement;
+
+	onMount(async () => {
+		await initialize();
+		await initUserStore();
+		await loginUser();
+
+		// Focus input
+		chatInput?.focus();
+	});
+
+	const onSubmitMessage = async () => {
+		if (message.trim().length === 0) {
+			return;
+		}
+
+		const savedMessageText = message;
+		message = '';
+
+		try {
+			await addMessage($userProfileStore.name, savedMessageText);
+		} catch (e) {
+			console.error('Error sending message', e);
+		}
+	};
+</script>
+
+<svelte:head>
+	<title>F-kit chat</title>
+</svelte:head>
+
+<GithubBanner url="https://github.com/Cinnamon-8/timetable-manager" />
+
+<h1>chat application</h1>
+
+<h2>
+	You are henceforth known as: <br />
+</h2>
+
+<div class="avatar">
+	{#if $userProfileStore?.avatar}
+		{@html $userProfileStore?.avatar}
+	{/if}
+</div>
+
+<h2>
+	{$userProfileStore?.name}
+</h2>
+
+<form on:submit|preventDefault={onSubmitMessage}>
+	<label hidden for="message">Message</label>
+	<input
+		type="text"
+		id="message"
+		name="message"
+		bind:value={message}
+		placeholder="What's on your mind?"
+		autocomplete="off"
+		required
+		bind:this={chatInput}
+	/>
+	<input type="submit" value="Send" />
+</form>
+
+<div>
+	{#each $chatMessagesStore as message (message.id)}
+		<Message {message} />
+	{/each}
+</div>
+
+<style>
+	:global(body) {
+		background-color: #8903ff ;
+		color: white;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+
+	form {
+		margin-bottom: 15px;
+	}
+
+	h1,
+	h2 {
+		text-align: center;
+	}
+
+	.avatar {
+		width: 128px;
+		height: 128px;
+		margin: 0 auto;
+	}
+
+	form {
+		text-align: center;
+	}
+  </style>
