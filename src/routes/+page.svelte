6ad724257758e3039e29207102f21a8da38dfa0c
Cinnamon-8 <121104003+Cinnamon-8@users.noreply.github.com>
2023-05-29 07:30:07 +0000

    page.svelte updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,4 @@
 <script>
 	import Auth from "../components/Auth.svelte";
-  import Calendar from "../components/Calendar.svelte";
-</script>
-<!-- <Calendar /> -->
-
-<Auth/> 
+  </script>
+ <Auth/> 
